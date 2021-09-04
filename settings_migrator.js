dbea5c163f734ea525598b3de07dbd8acd8a8752
KYGAS <45013496+KYGAS@users.noreply.github.com>
2021-09-04 23:39:47 +0200

    Major update; Introducing Sleep + Plague; Plague requires testing.
    For information or questions DM KYGAS#8575

--- settings_migrator.js
+++ settings_migrator.js
@@ -1,7 +1,7 @@
 "use strict"
 
 const DefaultSettings = {
-     "skills": {
+     "allySkills": {
         "6": [ // Priest
             19, // Focus Heal
             37  // Immersion
@@ -11,14 +11,26 @@ const DefaultSettings = {
             9  // Arun's Cleansing Touch
         ]
     },
-    
+	 "enemySkills": {
+        "6": [ // Priest
+			30, // Plague
+			33  // Sleep
+        ],
+        "7": [ // Mystic
+			24, // VoC
+			28, // Sleep
+			41 	// Contagion
+        ]
+    },
+	
     "autoCast": true,     // true = skills auto lockon and cast. false = pre-lockon onto targets and casting is done manually
     "autoHeal": true,     // enable healing skills
     "autoCleanse": true,  // enable mystic cleanse
+	"autoDebuff" : true,  // enable debuff lock-ons
     "hpCutoff": 97,       // (healing only) ignore members that have more HP% than this
-    "maxDistance": 35,    // in-game meters. can work up to 35m
-    "lockSpeed": 30,       // delay for locking on targets.
-    "castSpeed": 100       // delay for casting skills. castSpeed needs to be greater than lockSpeed.
+    "maxDistance": 30,    // in-game meters. can work up to 30m	
+    "lockSpeed": 30,      // delay for locking on targets.
+    "castSpeed": 50       // delay for casting skills. castSpeed needs to be greater than lockSpeed.
 }
 
 module.exports = function MigrateSettings(from_ver, to_ver, settings) {
@@ -42,6 +54,12 @@ module.exports = function MigrateSettings(from_ver, to_ver, settings) {
         switch(to_ver)
         {
 			// keep old settings, add new ones
+			case 2:
+				settings.version = 2;
+				settings.allySkills = DefaultSettings.allySkills;
+				settings.enemySkills = DefaultSettings.enemySkills;
+				settings.autoDebuff = DefaultSettings.autoDebuff;
+				break;
 			default:
 				let oldsettings = settings
 				
